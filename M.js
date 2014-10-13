9695a938bbe288cfe08825847ca3631469ea3191
Jascha Ehrenreich <jascha@jaeh.at>
2014-10-13 04:16:11 +0200

    fixed indent

--- M.js
+++ M.js
@@ -1,44 +1,44 @@
 'use strict';
 
 var express = require('express')
-	, fs = require('fs')
-	, vhost = require('vhost')
-	, path = require('path')
+  , fs = require('fs')
+  , vhost = require('vhost')
+  , path = require('path')
 
-	, lib = path.join(__dirname, 'lib')
+  , lib = path.join(__dirname, 'lib')
 
-	, autoload = require( path.join(lib, 'autoload') )
-	, errorHandler = require( path.join(lib, 'errorHandler') )
-	, U = require( path.join(lib, 'utils'))
+  , autoload = require( path.join(lib, 'autoload') )
+  , errorHandler = require( path.join(lib, 'errorHandler') )
+  , U = require( path.join(lib, 'utils'))
 
-	, M = express()
+  , M = express()
 ;
 
 M.set('port', ( process.env.PORT || 3000) );
 
 M.set('dirs', {
-	'hosts' : path.join(process.cwd(), 'hosts')
+  'hosts' : path.join(process.cwd(), 'hosts')
 });
 
 M.set('defaultHost', {
-		development: 'http://localhost:' + M.get('port')
-	, production : 'https://jaeh.at/'
+    development: 'http://localhost:' + M.get('port')
+  , production : 'https://jaeh.at/'
 } );
 
 autoload(M);
 
 M.on('mount', function (parent) {
-	U.log('M mounted');
+  U.log('M mounted');
 
-	M.use(errorHandler);
+  M.use(errorHandler);
 
-	M.use(function(req, res, next) {
-		res.redirect(M.get('defaultHost')[M.get('env')]);
-	});
+  M.use(function(req, res, next) {
+    res.redirect(M.get('defaultHost')[M.get('env')]);
+  });
 
-	M.listen(M.get('port'), function() {
-		U.log( 'M listening to port:', M.get('port') );
-	});
+  M.listen(M.get('port'), function() {
+    U.log( 'M listening to port:', M.get('port') );
+  });
 });
 
 module.exports = M;
