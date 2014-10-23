1c63b0558097f66a5c6b8d611de26b0ee51100da
Jascha Ehrenreich <jascha@jaeh.at>
2014-10-23 02:35:09 +0200

    moved log.js

--- M.js
+++ M.js
@@ -1,27 +1,31 @@
 'use strict';
 
-var express = require('express')
-  , fs = require('fs')
-  , vhost = require('vhost')
-  , path = require('path')
-
-  , lib = path.join(__dirname, 'lib')
-
-  , autoload = require( path.join(lib, 'autoload') )
+var express      = require('express')
+  , fs           = require('fs')
+  , vhost        = require('vhost')
+  , path         = require('path')
+  , lib          = path.join(__dirname, 'lib')
+  , autoload     = require( path.join(lib, 'autoload') )
   , errorHandler = require( path.join(lib, 'errorHandler') )
-  , U = require( path.join(lib, 'utils'))
 
   , M = express()
 ;
 
+M.log  = require( path.join(lib, 'log') );
+
+
 //default env is development
-M.set('env', ( M.get('env') || 'development') );
+M.set('env', ( M.get('env') || 'development' ) );
+
+if ( M.get('env') === 'development' ) {
+  M.cli  = require( path.join(lib, 'cli') );
+}
 
 M.set('port', ( process.env.PORT || 5000) );
 
 M.set('dirs', {
-  'hosts' : path.join(process.cwd(), 'hosts')
-});
+  'hosts' : path.join( process.cwd(), 'hosts' )
+} );
 
 M.set('defaultHost', {
     development: 'http://localhost:' + M.get('port')
@@ -29,19 +33,19 @@ M.set('defaultHost', {
 } );
 
 M.on('mount', function (parent) {
-  autoload(M);
+  M.log('M mounted');
+  
+  autoload(M, function() {
+    M.use(errorHandler);
 
-  U.log('M mounted');
+    M.use( function(req, res, next) {
+      res.redirect(M.get('defaultHost')[M.get('env')]);
+    } );
 
-  M.use(errorHandler);
-
-  M.use(function(req, res, next) {
-    res.redirect(M.get('defaultHost')[M.get('env')]);
-  });
-
-  M.listen(M.get('port'), function() {
-    U.log( 'M listening to port:', M.get('port') );
-  });
+    M.listen( M.get('port'), function() {
+      M.log( 'M listening to port:', M.get('port') );
+    } );
+  } );
 });
 
 module.exports = M;
