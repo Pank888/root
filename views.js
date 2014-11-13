3507a54e97e79730c5163f4406f48e899f7b9c23
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-13 06:05:39 +0100

    changed object structure views.render.page

--- views.js
+++ views.js
@@ -1,30 +1,38 @@
 'use strict';
 var log = require('magic-log');
 
-exports.renderPage = function renderPage(data, req, res, next) {
-  var page     = ( req.params.page || 'index' )
-    , template = 'pages/' + page
-    , app      = req.app
-    , pages    = app.get('pages') || [];
+exports.render = {
+  page: function renderPage(req, res, next) {
+    var page     = ( req.params.page || 'index' )
+      , template = 'pages/' + page
+      , app      = req.app
+      , pages    = app.get('pages') || []
+      , data     = {}
+    ;
 
-  //on first request the html gets cached
-  if ( pages && pages[page] ) {
-    return res.send(pages[page]);
-  }
+    log('Rendering Page: ', page);
 
-  res.render(template, data, function (err, html) {
-    if ( err ) {
-      log.error(err);
-      if ( req.params.page === '404' ) {
-        return next(err, req, res, next);
+    //on first request the html gets cached
+    if ( pages && pages[page] ) {
+      return res.send(pages[page]);
+    }
+
+    res.render(template, data, function (err, html) {
+      if ( err ) { log.error(err); }
+      if ( ! html ) {
+        if ( page === '404' ) {
+          log.error('magic-views tried loading a 404.jade template from a views dir and failed.');
+          return next(err, req, res, next);
+        }
+        if ( page === 'index' ) {
+          log.error('magic-views tried to render an index.jade template from a views dir and failed.');
+        }
+        return res.redirect('/404');
       }
 
-      return res.redirect('/404');
-    } else if ( html ) {
-      //caching html of pages
-      pages[page] = html;
-      app.set('pages', pages);
+      pages[page] = html; 
+      app.set('pages', pages); //caching pages
       res.send(html);
-    }
-  });
-}
+    });
+  }
+};
