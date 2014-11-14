e9a95afd1a43e34119c21230f20b4e6b2f4804b3
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-14 22:03:52 +0100

    removed unneeded variables

--- views.js
+++ views.js
@@ -6,14 +6,10 @@ exports.render = {
     var page     = ( req.params.page || 'index' )
       , app      = req.app
       , template = 'pages/' + page
-      , pages    = app.get('pages') || []
-      , data     = {}
-      , views    = app.get('views')
     ;
+    log('magic-views', 'Rendering Page:', page);
 
-    log('Rendering Page:', page, 'with template:', template);
-
-    res.render(template, data, function (err, html) {
+    res.render(template, function (err, html) {
       if ( err ) { return next(err); }
       if ( ! html ) { return next();}
       res.send(html);
