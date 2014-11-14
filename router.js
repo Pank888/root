ed0614d9c6bd75f340832a351414f3643402ea6d
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-14 22:07:56 +0100

    added logging

--- router.js
+++ router.js
@@ -1,14 +1,16 @@
 var express = require('express')
+  , router  = express.Router()
   , log     = require('magic-log')
   , view    = require('magic-view')
-  , router  = express.Router()
 ;
 
 router.get('/', function(req, res, next) {
+  log.info('magic-router', '/ route called');
   view.render.page(req, res, next);
 });
 
 router.get('/:page', function(req, res, next) {
+  log.info('magic-router', '/:page route called', req.params.page);
   view.render.page(req, res, next);
 });
 
