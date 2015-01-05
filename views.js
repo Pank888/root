dafd83ff469e604fee22c0a40c1ae690f17a8f89
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-05 15:34:26 +0100

    removed some logs, trigger 404 instead of 505 in subpage

--- views.js
+++ views.js
@@ -2,15 +2,31 @@
 var log = require('magic-log');
 
 exports.render = {
-  page: function renderPage(req, res, next) {
-    var page     = ( req.params.page || 'index' )
-      , app      = req.app
-      , template = 'pages/' + page
+    page: function renderPage(req, res, next) {
+      var page     = ( req.params.page || 'index' )
+        , app      = req.app
+        , template = 'pages/' + page
+      ;
+      log('magic-views', 'Rendering Page:', page);
+
+      res.render(template, function (err, html) {
+        if ( err || ! html ) { return next(err); }
+        log('magic-view', 'Sending response');
+        res.send(html);
+      });
+    }
+  , subPage: function renderSubPage(req, res, next) {
+    var page = ( req.params.page || 'index' )
+      , dir  = ( req.params.dir || false )
+      , app  = req.app
+      , template = 'pages/' + dir + '/' + page
     ;
-    log('magic-views', 'Rendering Page:', page);
+    if ( ! dir || ! page ) { return next(); }
+    console.log('magic-views', 'rendering dir', dir, 'rendering subpage', page);
 
     res.render(template, function (err, html) {
-      if ( err || ! html ) { return next(); }
+      if ( err || ! html ) { return next(); } //404
+      log('magic-view', 'Sending response');
       res.send(html);
     });
   }
