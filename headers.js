c29110c5c66758221aad5d723da1f835d360d606
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-05 16:11:23 +0100

    disabled cache on development

--- headers.js
+++ headers.js
@@ -1,11 +1,12 @@
 'use strict';
 //middleware function
 module.exports = function (req, res, next) {
-  var maxAge = 60 * 60 * 24 * 7;
-
+  var env = req.app.get('env') || 'production';
+  if ( env !== 'development' ) {
+    var maxAge = 60 * 60 * 24 * 7;
+    res.set('Cache-Control', 'public, max-age=' + maxAge); // 4 days
+    res.set('Expires', new Date(Date.now() + (maxAge * 1000)).toUTCString());  
+  }
   res.set('X-Powered-By', 'magic');
-  res.set('Cache-Control', 'public, max-age=' + maxAge); // 4 days
-  res.set('Expires', new Date(Date.now() + (maxAge * 1000)).toUTCString());  
-
   next();
 }
