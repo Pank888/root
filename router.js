dd428a0af7804a9dc6a13aaab0327a936554c624
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-05 16:15:13 +0100

    readded subpages

--- router.js
+++ router.js
@@ -5,7 +5,7 @@ var express = require('express')
 ;
 
 router.get('/', view.render.page);
-//~ router.get('/:dir/:page', view.render.subPage);
+router.get('/:dir/:page', view.render.subPage);
 router.get('/:page', view.render.page);
 
 module.exports = router;
