7730fddbda8032fbab9fcff37158c286aa506d20
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-06 02:03:42 +0100

    added user config

--- headers.js
+++ headers.js
@@ -1,12 +1,17 @@
 'use strict';
 //middleware function
 module.exports = function (req, res, next) {
-  var env = req.app.get('env') || 'production';
+  var app       = req.app
+    , env       = app.get('env') || 'production';
+    , poweredBy = app.get('X-Powered-By') || 'Magic'
+    , maxAge    = app.get('maxAge') || 60 * 60 * 24 * 7 //default to 7 days
+  ;
+  
   if ( env !== 'development' ) {
-    var maxAge = 60 * 60 * 24 * 7;
-    res.set('Cache-Control', 'public, max-age=' + maxAge); // 4 days
+    res.set('Cache-Control', 'public, max-age=' + maxAge);
     res.set('Expires', new Date(Date.now() + (maxAge * 1000)).toUTCString());  
   }
-  res.set('X-Powered-By', 'magic');
+
+  res.set('X-Powered-By', poweredBy);
   next();
 }
