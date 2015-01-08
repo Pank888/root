aa513be53c492f9be3aaec766eec4438461c8610
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-09 00:45:36 +0100

    gosh, need to get npm link into the buildchain already for bughunts like this

--- view.js
+++ view.js
@@ -8,6 +8,9 @@ function renderPage(res, template, next) {
   res.render(template, function (err, html) {
     log('magic-view', 'rendered page');
     if ( err ) { log.error('magic-view', 'error in res.render', err); }
+    if ( ! html ) { 
+      log.error('magic-view', 'html file was empty for template', template);
+    }
     if ( err || ! html ) { return next(); } //404, no error passing!
     log('magic-view', 'Sending response');
     res.status(200).send(html);
