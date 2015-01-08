6c0252170cd656ce354f07f158dfbe3806588eae
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-08 21:09:13 +0100

    added res.locals.page, added renderPage function

--- views.js
+++ views.js
@@ -1,6 +1,16 @@
 'use strict';
 var log = require('magic-log');
 
+function renderPage(res, page, template, next) {
+  res.locals.page = page;
+  res.render(template, function (err, html) {
+    if ( err || ! html ) { return next(); } //404, no error passing!
+    log('magic-view', 'Sending response');
+    res.status(200).send(html);
+  });
+
+}
+
 exports.render = {
     page: function renderPage(req, res, next) {
       var page     = ( req.params.page || 'index' )
@@ -8,12 +18,7 @@ exports.render = {
         , template = 'pages/' + page
       ;
       log('magic-views', 'Rendering Page:', page, 'with template', template);
-
-      res.render(template, function (err, html) {
-        if ( err || ! html ) { return next(); } //404, no error passing!
-        log('magic-view', 'Sending response');
-        res.status(200).send(html);
-      });
+      renderPage(res, page, template, next);
     }
   , subPage: function renderSubPage(req, res, next) {
       var page = ( req.params.page || 'index' )
@@ -25,10 +30,6 @@ exports.render = {
       if ( ! dir || ! page ) { return next(); }
       log('magic-views', 'rendering dir:', dir, 'rendering subpage', template);
 
-      res.render(template, function (err, html) {
-        if ( err || ! html ) { return next(); } //404, no error passing!
-        log('magic-view', 'Sending response');
-        res.status(200).send(html);
-      });
+      renderPage(res, page, template, next);
     }
 };
