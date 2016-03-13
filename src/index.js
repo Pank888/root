8180b7c5e11e1aa5fb69d702393e5bc9818ebb38
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-14 00:03:01 +0100

    es7ify

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-'use strict';
-//middleware function
-module.exports = function (req, res, next) {
-  var app       = req.app
-    , env       = app.get('env') || 'production'
-    , poweredBy = app.get('X-Powered-By') || 'Magic'
-    , maxAge    = app.get('maxAge') || 60 * 60 * 24 * 7 //default to 7 days
-  ;
-  
-  if ( env !== 'development' ) {
-    res.set('Cache-Control', 'public, max-age=' + maxAge);
-    res.set('Expires', new Date(Date.now() + (maxAge * 1000)).toUTCString());  
-  }
+const noop = () => {};
 
-  res.set('X-Powered-By', poweredBy);
-  next();
-}
+// http header middleware function
+export const headers =
+  (req, res, next = noop) => {
+    const app = req.app;
+    const env = app.get('env') || 'production';
+    const poweredBy = app.get('X-Powered-By') || 'Magic';
+    const maxAge = app.get('maxAge') || 60 * 60 * 24 * 7; // default to 7 days
+
+    if (env !== 'development') {
+      res.set('Cache-Control', `public, max-age=${maxAge}`);
+      res.set('Expires', new Date(Date.now() + (maxAge * 1000)).toUTCString());
+    }
+
+    res.set('X-Powered-By', poweredBy);
+    next();
+  };
+
+export default headers;
