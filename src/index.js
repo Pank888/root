94d4b028cc2094e2503eb630d7081ef9976db880
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-14 00:46:22 +0100

    es7ify

--- src/index.js
+++ src/index.js
@@ -1,30 +1,34 @@
-import express from 'express';
-import {Router as handler} from 'express';
-import {log, warn, error} from 'magic-log';
+import log from 'magic-server-log';
 import { page } from 'magic-pages';
 
-export function handle404(req, res, next) {
-  var app  = req.app
-    , p404 = app.get('404page') || '404'
-    , r404 = app.get('404redirect') || false
-  ;
-  if ( r404 ) {
-    log(`magic-errorHandler r404 was set in host, redirect: ${r404}`);
-    return res.redirect(r404);
-  }
+const noop = () => {};
 
-  warn(`404 error page called, page was: ${req.params.page}`);
-  req.params.page = p404;
-  res.status(404);
-  page(req, res, (err) => {
-    if ( err ) {
-      error(`404 page template for host: ${req.hostname} not found`);
-      return next(err);
+export const handle404 =
+  (req, res, next = noop) => {
+    const app = req.app;
+    const p404 = app.get('404page') || '404';
+    const r404 = app.get('404redirect') || false;
+
+    log.warn(`404 error page called, page was: ${req.params.page}`);
+
+    if (r404) {
+      log(`magic-errorHandler r404 was set in host, redirect: ${r404}`);
+      return res.redirect(r404);
     }
-  });
-}
 
-export function handle500(err, req, res, next) {
-  error(`500 called, err: ${err}`);
-  res.send('500 server error');
-}
+    req.params.page = p404;
+    res.status(404);
+
+    page(req, res, err => {
+      if (err) {
+        log.error(`404 page template for host: ${req.hostname} not found`);
+        return next(err);
+      }
+    });
+  };
+
+export const handle500 =
+  (err, req, res, next) => {
+    log.error(`500 called, err: ${err}`);
+    res.send('500 server error');
+  };
