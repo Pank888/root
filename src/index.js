695bb84ea0b6cdae1373a568cf92f5ac3fb85bcc
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-14 00:29:44 +0100

    es7ify

--- src/index.js
+++ src/index.js
@@ -1,73 +1,86 @@
-import {log, error, success} from 'magic-log';
+import log from 'magic-log';
 
-export function renderTemplate(res, template, next) {
-  log(`magic-view: rendering template ${template}`);
-  res.render(template, (err, html) => {
-    if ( err ) { error(`magic-view: error in res.render ${err}`); }
-    if ( ! html ) { 
-      error(`magic-view: html file was empty for template ${template}`);
-    }
-    if ( err || ! html ) { return next(); } //404, no error passing!
-    res.status(200).send(html);
-  });
-}
+const libName = 'magic-pages';
 
-function getPage(req, res) {
-  if ( res.locals.page ) { return res.locals.page; }
-  return (req.params && req.params.page ) ? req.params.page : 'index';
-}
+const noop = () => {};
 
-
-function getTemplate(req, res) {
-  var page     = getPage(req, res)
-    , template = 'pages/'
-  ;
-
-  if ( req.params && req.params.dir ) {
-    template += req.params.dir + '/';
-  }
-  
-  template += page;
-
-  log('magic-view', 'Rendering Page:', page, 'with template', template);
-  res.locals.page = page;
-  res.locals.template = template;
-  return template;
-}
-
-function getPageSlug(req) {
-  return (req.params && req.params.page ) ? req.params.page : 'index';
-}
-
-function getPageParentSlug(req) {
-  return (req.params && req.params.dir ) ? req.params.dir : false;
-}
-
-export function page(req, res, next) {
-  var template = getTemplate(req, res)
-    , db       = req.app.get('db')
-  ;
-  if ( ! db || ! db.pages ) {
-    renderTemplate(res, template, next);
-  } else {
-    success('db.pages is set');
-    var parentSlug = getPageParentSlug(req)
-      , pageQuery = {
-        slug: getPageSlug(req)
+export const renderTemplate =
+  (res, template, next = noop) => {
+    log(`${libName}: rendering template ${template}`);
+    res.render(template, (err, html) => {
+      if (err) { log.error(`magic-view: error in res.render ${err}`); }
+      if (!html) {
+        log.error(`${libName}: html file was empty for template ${template}`);
       }
-    ;
-    if ( parentSlug ) {
+      if (err || !html) { return next(); } // 404, no error passing!
+      res.status(200).send(html);
+    });
+  };
+
+const getPage =
+  (req, res) => {
+    if (res.locals.page) { return res.locals.page; }
+    return (req.params && req.params.page) ? req.params.page : 'index';
+  };
+
+const getTemplate =
+  (req, res) => {
+    const page = getPage(req, res);
+    let template = 'pages/';
+
+    if (req.params && req.params.dir) {
+      template += req.params.dir + '/';
+    }
+
+    template += page;
+
+    log(`${libName} Rendering Page: ${page} with template ${template}`);
+    res.locals.page = page;
+    res.locals.template = template;
+    return template;
+  };
+
+const getPageSlug =
+  req =>
+    req.params && req.params.page
+      ? req.params.page
+      : 'index';
+
+const getPageParentSlug =
+  req =>
+    req.params && req.params.dir
+      ? req.params.dir
+      : false;
+
+export const page =
+  (req, res, next = noop) => {
+    const template = getTemplate(req, res);
+    const db = req.app.get('db');
+
+    if (!db || !db.pages) {
+      return renderTemplate(res, template, next);
+    }
+
+    log.success('db.pages is set');
+    const parentSlug = getPageParentSlug(req);
+    const pageQuery = {
+      slug: getPageSlug(req),
+    };
+
+    if (parentSlug) {
       pageQuery.parent = parentSlug;
     }
 
     db.pages.findOne(pageQuery, (err, page) => {
-      if (err) { return error(err); }
+      if (err) {
+        return log.error(err);
+      }
 
-      if ( ! page ) {
+      if (!page) {
         return renderTemplate(res, template, next);
       }
 
+      // TODO: actually render db page
       console.log(page);
     });
-  }
-}
+  };
