aa2c0dbf37a571710fdac25176adf09d896e63fe
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-15 15:53:46 +0100

    remove unported packages, refactor

--- src/index.js
+++ src/index.js
@@ -1,139 +1,153 @@
-'use strict';
 import express from 'express';
 import basicAuth from 'node-basicauth';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
-import {existsSync} from 'fs';
-import {init as initAdmin} from 'magic-admin';
-import blog from 'magic-blog';
-import db from 'magic-db';
-import {handle404, handle500} from 'magic-errorHandler';
-import headers from 'magic-headers';
-import {log} from 'magic-log';
+import { existsSync } from 'fs';
+// import { init as initAdmin } from 'magic-admin';
+// import blog from 'magic-blog';
+// import db from 'magic-db';
+import { isArray, isObject } from 'magic-types';
+import { handle404, handle500 } from 'magic-errorHandler';
+import headers from 'magic-http-headers';
 import router from 'magic-router';
-import {each} from 'magic-utils';
 import morgan from 'morgan';
-import {join} from 'path';
+import { join } from 'path';
 import favicon from 'serve-favicon';
 import stylus from 'stylus';
 import nib from 'nib';
 
-module.exports = (M, app, dir) => {
-  var css         = ( app.get('css') || stylus )
-    , env         = app.get('env') || 'production'
-    , faviconPath = join(dir, 'public', 'favicon.ico')
-    , dbSettings  = app.get('dbOpts') || false
-    , dirs        = app.get('dirs') || {
-        public: join(dir, app.get('publicDir') || 'public')
-      , root  : dir
-      , cache : join(dir, app.get('cacheDir') || '.cache')
-      , views : join(dir, app.get('viewsDir') || 'views')
-    }
-  ;
-  app.set('css', css);
-  app.set('dirs', dirs);
+export default
+  (M, app, dir) => {
+    const css = app.get('css') || stylus;
+    const env = app.get('env') || 'production';
+    const faviconPath = join(dir, 'public', 'favicon.ico');
+    // const dbSettings = app.get('dbOpts') || false;
 
-  app.use( (req, res, next) => {
-    req.app = app;
-    next();
-  });
+    const dirs = {
+      root: dir,
+      public: join(dir, app.get('publicDir') || 'public'),
+      cache: join(dir, app.get('cacheDir') || '.cache'),
+      views: join(dir, app.get('viewsDir') || 'views'),
+      ...app.get('dirs'),
+    };
 
-  //set expiry headers
-  app.use(headers);
+    app.set('css', css);
+    app.set('dirs', dirs);
 
-  if ( app.get('basicAuth') ) {
-    app.use( basicAuth( app.get('basicAuth') ) );
-  }
-
-  //fs.existsSync only gets called once on first request
-  if ( ! app.get('faviconChecked') && ! app.get('faviconExists') ) {
-    app.set('faviconChecked', true);
-    app.set('faviconExists', existsSync(faviconPath));
-  }
-
-  if ( app.get('faviconExists') ) {
-    app.use( favicon(faviconPath) );
-  }
-
-  app.set('views', dirs.views);
-  app.set('view engine', app.get('view engine') || 'jade');
-
-  app.use(compression({ threshold: 128 }));
-
-  app.use( css.middleware({
-      src: dirs.public
-    , maxage: '1d'
-    , compile: (str, path) => {
-        return css(str)
-                .set('filename', path)
-                .set('compress', app.get('env') === 'production' )
-                .use(nib())
-                .import('nib')
-        ;
-      }
-  }) );
-
-  app.use( express.static(join(__dirname, 'public'), {maxAge: '1w'}) );
-
-  app.use( express.static(dirs.public, {maxAge: '1d'}) );
-
-  if ( dbSettings && ! app.get('db') ) {
-    app.use(db);
-  }
-
-  if ( app.enabled('admin') ) {
-    app.use( initAdmin );
-  }
-
-  if ( app.get('blogRoot') ) {
-    let blogRoot = app.get('blogRoot');
-    if ( typeof blogRoot !== 'string' && typeof blogRoot !== 'number' ) {
-      blogRoot = 'blog';
-    }
-    if ( blogRoot.charAt(0) !== '/' ) {
-      blogRoot = '/' + blogRoot;
-    } else {
-      app.set('blogRoot', blogRoot.substr(1) );
-    }
-    app.use( blogRoot, blog );
-  }
-
-  //logging
-  app.use(morgan('combined'));
-
-  //if host sets bodyparser to true, init it
-  if ( app.enabled('bodyParser') ) {
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-  }
-
-  //if host sets cookieparser to true, init it:
-  if ( app.enabled('cookieParser') ) {
-    app.use(cookieParser());
-  }
-
-  //load host specific router
-  if ( app.get('router') ) {
-    let routes = app.get('router');
-
-    if ( typeof routes === 'array' || typeof routes === 'object' ) {
-      each(routes, route => {
-        app.use(route);
+    app.use(
+      (req, res, next) => {
+        req.app = app;
+        next();
       });
-    } else if ( typeof routes === 'function' ) { 
-      app.use(routes);
+
+    // set expiry headers
+    const customHeaders = {
+      ...app.get('headers'),
+      ...headers,
+    };
+    app.use(customHeaders);
+
+    const basicAuthConfig = app.get('basicAuth');
+    if (basicAuthConfig) {
+      app.use(basicAuth(basicAuthConfig));
     }
-  }
 
-  //default router
-  app.use(router);
+    // fs.existsSync only gets called once on first request
+    if (!app.get('faviconChecked') && !app.get('faviconExists')) {
+      app.set('faviconChecked', true);
+      app.set('faviconExists', existsSync(faviconPath));
+    }
 
-  //we are in a 404 error
-  app.use(handle404);
+    if (app.get('faviconExists')) {
+      app.use(favicon(faviconPath));
+    }
 
-  //oops, worst case fallback, 500 server error.
-  app.use(handle500);
+    app.set('views', dirs.views);
+    app.set('view engine', app.get('view engine') || 'jade');
 
-  return app;
-}
+    app.use(compression({ threshold: 128 }));
+
+    const cssMiddleware =
+      (str, path) =>
+        css(str)
+          .set('filename', path)
+          .set('compress', env === 'production')
+          .use(nib())
+          .import('nib');
+
+    app.use(css.middleware({
+      src: dirs.public,
+      maxage: '1d',
+      compile: cssMiddleware,
+    }));
+
+    app.use(express.static(join(__dirname, 'public'), { maxAge: '1w' }));
+
+    app.use(express.static(dirs.public, { maxAge: '1d' }));
+
+    // if (dbSettings && !app.get('db')) {
+    //   app.use(db);
+    // }
+
+    // if (app.enabled('admin')) {
+    //   app.use(initAdmin);
+    // }
+
+    /*
+    TODO: reenable
+    if (app.get('blogRoot')) {
+      let blogRoot = app.get('blogRoot');
+      if (typeof blogRoot !== 'string' && typeof blogRoot !== 'number') {
+        blogRoot = 'blog';
+      }
+      if (blogRoot.charAt(0) !== '/') {
+        blogRoot = '/' + blogRoot;
+      } else {
+        app.set('blogRoot', blogRoot.substr(1));
+      }
+      app.use(blogRoot, blog);
+    }
+    */
+
+    const logLevel = app.get('logLevel') || 'combined';
+
+    // logging
+    app.use(morgan(logLevel));
+
+    // if host sets bodyparser to true, init it
+    if (app.enabled('bodyParser')) {
+      app.use(bodyParser.json());
+      app.use(bodyParser.urlencoded({ extended: false }));
+    }
+
+    // if host sets cookieparser to true, init it:
+    if (app.enabled('cookieParser')) {
+      app.use(cookieParser());
+    }
+
+    // load host specific router
+    if (app.get('router')) {
+      const routes = app.get('router');
+
+      if (isArray(routes) || isObject(routes)) {
+        Object.keys(routes).forEach(
+          key =>
+            app.use(routes[key])
+          );
+      } else if (typeof routes === 'function') {
+        app.use(routes);
+      }
+    }
+
+    // default router
+    app.use(router);
+
+    // we are in a 404 error
+    app.use(handle404);
+
+    // oops, worst case fallback, 500 server error.
+    app.use(handle500);
+
+    return app;
+  };
