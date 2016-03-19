a807fc73422bb5cfbed72ef035420140047e1b27
Jascha Ehrenreich <jascha@jaeh.at>
2016-03-19 22:39:39 +0100

    update function call to remote var assignment

--- src/headers.js
+++ src/headers.js
@@ -2,8 +2,7 @@ const noop = () => {};
 
 // http header middleware function
 export const headers =
-  (req, res, next = noop) => {
-    const app = req.app;
+  ({ app = {}}, res, next = noop) => {
     const env = app.get('env') || 'production';
     const poweredBy = app.get('X-Powered-By') || 'Magic';
     const maxAge = app.get('maxAge') || 60 * 60 * 24 * 7; // default to 7 days
