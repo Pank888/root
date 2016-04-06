69bdbd66db99a0724e11a6ca81b08492161b8930
Jascha Ehrenreich <jascha@jaeh.at>
2016-04-06 15:39:44 +0200

    update headers

--- dist/headers.js
+++ dist/headers.js
@@ -3,6 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var noop = function noop() {};
 
 // http header middleware function
@@ -14,13 +17,24 @@ var headers = exports.headers = function headers(_ref, res) {
   var env = app.get('env') || 'production';
   var poweredBy = app.get('X-Powered-By') || 'Magic';
   var maxAge = app.get('maxAge') || 60 * 60 * 24 * 7; // default to 7 days
+  var appHeaders = app.get('headers') || {};
+
+  var headers = {};
 
   if (env !== 'development') {
-    res.set('Cache-Control', 'public, max-age=' + maxAge);
-    res.set('Expires', new Date(Date.now() + maxAge * 1000).toUTCString());
+    headers['Expires'] = new Date(Date.now() + maxAge * 1000).toUTCString();
+    headers['Cache-Control'] = 'public, max-age=' + maxAge;
   }
 
-  res.set('X-Powered-By', poweredBy);
+  headers['X-Powered-By'] = poweredBy;
+  headers['server'] = poweredBy;
+
+  headers = _extends({}, headers, appHeaders);
+
+  Object.keys(headers).forEach(function (key) {
+    return res.set(key, headers[key]);
+  });
+
   next();
 };
 
